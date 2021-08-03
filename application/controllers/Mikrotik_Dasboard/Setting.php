360b6daf0acce2f628e4adce0eb86d71ec7bd4ce
Ranur-react <rahmatnur844@gmail.com>
2021-08-03 15:46:06 +0700

    Setting

--- application/controllers/Mikrotik_Dasboard/Setting.php
+++ application/controllers/Mikrotik_Dasboard/Setting.php
@@ -17,10 +17,15 @@ class Setting extends CI_Controller
   public function index()
   {
       $bridgename='bridgelocal';
+      $ip='192.168.200.1/24';
+      $etherWAN='ether1';
    $this->MembuatBridgebaru($bridgename);
+//    $this->MenambahportBridgeLocal($bridgename,'ether2');
    $this->MenambahportBridgeLocal($bridgename,'ether3');
    $this->MenambahportBridgeLocal($bridgename,'ether4');
-   $this->MenambahportBridgeLocal($bridgename,'ether5');
+   $this->MenambahportBridgeLocal($bridgename,'wlan1');
+   $this->Settingip($bridgename,$ip);
+   $this->SettingWANdanRouteAuto($etherWAN);
   }
   public function MembuatBridgebaru($bridgename)
   {
@@ -39,6 +44,35 @@ class Setting extends CI_Controller
       'interface' => $ether,
       'bridge'=>$bridgename,
     );;
+    $this->mikapi->write($Code, $data);
+  }
+  public function Settingip($bridgename,$ip)
+  {
+    $data = $this->DbLogin->show();
+    $Code['command'] = "/ip/address/add"; //perntah
+    $Code['ArrayValue'] = array(         //value dari perintah
+      'address' => $ip,
+      'interface'=>$bridgename,
+    );;
+    $this->mikapi->write($Code, $data);
+  }
+  public function SettingWANdanRouteAuto($ether)
+  {
+    $data = $this->DbLogin->show();
+    $Code['command'] = "/ip/dhcp-client/add"; //perntah
+    $Code['ArrayValue'] = array(         //value dari perintah
+      'interface' => $ether,
+    );
+    $this->mikapi->write($Code, $data);
+    $this->enableSettingWANdanRouteAuto();
+  }
+  public function enableSettingWANdanRouteAuto()
+  {
+    $data = $this->DbLogin->show();
+    $Code['command'] = "/ip/dhcp-client/enable"; //perntah
+    $Code['ArrayValue'] = array(         //value dari perintah
+      'number' => 0,
+    );
     $this->mikapi->write($Code, $data);
   }
 }
